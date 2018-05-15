9df3c72cde37427209ce7585d6a5b01e29a82af4
Floyd Kots <floydkotohoyoh@gmail.com>
2018-05-15 09:44:39 +0300

    Add DownloadingImages Demo

---
+++ DownloadingImages/app/src/main/java/pro/kots/downloadingimages/ImageDownloader.java
@@ -0,0 +1,30 @@
+package pro.kots.downloadingimages;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+    @Override
+    protected Bitmap doInBackground(String... urls) {
+        try {
+            URL url = new URL(urls[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();
+            return BitmapFactory.decodeStream(inputStream);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- DownloadingImages/app/src/main/java/pro/kots/downloadingimages/MainActivity.java
+++ DownloadingImages/app/src/main/java/pro/kots/downloadingimages/MainActivity.java
@@ -1,13 +1,31 @@
 package pro.kots.downloadingimages;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png
+    ImageView downloadedImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadedImg = findViewById(R.id.imageView);
+    }
+
+    public void downloadImage(View view) {
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png").get();
+            downloadedImg.setImageBitmap(myImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
