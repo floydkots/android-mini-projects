ab628beb3fd6d88a52cfb772d2fe4b1c557ed60a
Floyd Kots <floydkotohoyoh@gmail.com>
2018-05-21 14:59:05 +0300

    Integrated Facebook Comment

--- VersionControlDemo/app/src/main/java/pro/kots/versioncontroldemo/MainActivity.java
+++ VersionControlDemo/app/src/main/java/pro/kots/versioncontroldemo/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String user = "Rob";
+
+        // Facebook integration code
     }
 }
