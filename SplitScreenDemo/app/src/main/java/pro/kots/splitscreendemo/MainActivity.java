c0a2d4d9ef1d6a847a282e197dc52bfdb82b76e5
Floyd Kots <floydkotohoyoh@gmail.com>
2018-05-21 17:18:41 +0300

    Add Split Screen Demo

---
+++ SplitScreenDemo/app/src/main/java/pro/kots/splitscreendemo/MainActivity.java
@@ -0,0 +1,27 @@
+package pro.kots.splitscreendemo;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onMultiWindowModeChanged(boolean isInMultiWindowMode, Configuration newConfig) {
+        super.onMultiWindowModeChanged(isInMultiWindowMode, newConfig);
+        // Multi-window mode has changed
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isInMultiWindowMode()) {
+            // We're in multi window mode
+        }
+
+        if (isInPictureInPictureMode()) {
+            // We're in picture in picture mode
+        }
+    }
+}
